88a6594f95b5d63c057efafd9c0783430bf7db14
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2016-03-25 16:28:05 +0100

    host

--- src/main/java/nl/trivento/lambda/JavaTest.java
+++ src/main/java/nl/trivento/lambda/JavaTest.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.InetAddress;
 
 
 public class JavaTest {
   private int called = 0;
   public void greeting(InputStream input, OutputStream output, Context context) throws IOException {
-    String result = "Greetings. [" + called++ + "] " + context.getAwsRequestId();
+
+    String result = "Greetings. " + InetAddress.getLocalHost().getHostAddress() + " [" + called++ + "] " + context.getAwsRequestId() + "> " + System.getenv();
     output.write(result.getBytes("UTF-8"));
   }
 
